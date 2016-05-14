4a34bbc9bf9bee9b0d208db484e4290fbe6b5c75
sudeepdoon <sudeepmanchanda@outlook.com>
2016-05-14 17:22:56 +0530

    Creating new repo

---
+++ USR/platforms/android/assets/www/js/routes.js
@@ -0,0 +1,38 @@
+﻿angular.module('starter.routes', [])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+
+    .state('home', {
+        url: '/home',
+        templateUrl: 'home.html'
+    })
+
+    .state('dailyThoughts', {
+        url: '/dailyThoughts',
+        templateUrl: 'dailyThoughts.html',
+        controller: function ($scope) {
+            $scope.years = [{ 'name': '2016' },
+                            { 'name': '2015'},
+                            { 'name': '2014' }
+                            ];
+        }
+    })
+
+    .state('month', {
+        url: '/dailyThoughts/:year',
+        templateUrl: 'thoughtsMonth.html',
+        controller: function ($scope, $stateParams) {
+            $scope.year = $stateParams.year;
+            $scope.months = [
+                            {'name': 'December'},
+                            {'name': 'November'},
+                            { 'name': 'October' }
+                            ];
+        }
+    });
+
+});
